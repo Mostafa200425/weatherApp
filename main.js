b5e6bfdc62f73c805208d73b9e9ce6b9a8fdd8b0
Mostafa200425 <109903163+Mostafa200425@users.noreply.github.com>
2024-09-12 22:16:16 +0300

    Weather application has successfully completed
    This is my weather app using Html5, CSS, JS

---
+++ main.js
@@ -0,0 +1,51 @@
+const apiKey = "a5fcce8f6f1f27e03e5db66422ee0b99";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+let cityName = "&q=cairo";
+let inputName = document.querySelector(".searchBox");
+let weatherStatusIcon = document.querySelector(".icon");
+let errorMsg = document.querySelector(".error");
+
+async function checkWeather(cityName) {
+    const response = await fetch(apiUrl + `&appid=${apiKey}` + cityName);
+
+    if (response.status == 404) {
+        errorMsg.style.display = "block";
+    } else {
+        var data = await response.json();
+        document.querySelector(".temp").textContent = Math.round(data.main.temp) + "°C";
+        document.querySelector(".cityName").textContent = data.name;
+        document.querySelector(".humidity").textContent = data.main.humidity + "%";
+        document.querySelector(".wind").textContent = data.wind.speed + " km/h";
+
+        if (data.weather[0].main === "Clouds") {
+            weatherStatusIcon.src = "assets/clouds.png";
+        } else if (data.weather[0].main === "Clear") {
+            weatherStatusIcon.src = "assets/clear.png";
+        } else if (data.weather[0].main === "Rain") {
+            weatherStatusIcon.src = "assets/rain.png";
+        } else if (data.weather[0].main === "Mist") {
+            weatherStatusIcon.src = "assets/mist.png";
+        } else if (data.weather[0].main === "Drizzle") {
+            weatherStatusIcon.src = "assets/drizzle.png";
+        }
+        errorMsg.style.display = "none";
+        saveCityName(cityName);
+    }
+}
+
+function saveCityName(cityName) {
+    localStorage.setItem("cityName", cityName);
+}
+
+window.onload = function () {
+    const savedCity = localStorage.getItem("cityName");
+    if (savedCity) {
+        cityName = savedCity;
+    }
+    checkWeather(cityName);
+};
+
+document.querySelector(".searchButton").onclick = function () {
+    cityName = `&q=${inputName.value}`;  
+    checkWeather(cityName);  
+};
